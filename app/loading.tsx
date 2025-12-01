4bdcf658d3b83b46c8b7cc51fe893c09b33dc025
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2025-12-01 10:59:45 -0500

    fixed loading position

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,20 +1,22 @@
-'use client';
+"use client";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const override = {
-    display: 'block',
-    marging: '100px auto'
-}
+  display: "block",
+  marging: "100px auto",
+};
 
 const Loading = () => {
-    return (
-        <ClipLoader 
-            color="#3B82F6" 
-            cssOverride={override} 
-            size={150} 
-            aria-label='Loading Spinner' 
-        />
-    )
-}
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <ClipLoader
+        color="#3B82F6"
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+      />
+    </div>
+  );
+};
 
 export default Loading;
