59fed7aee616927b3fb0cc8c208e3aa2c7de67b1
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2025-12-01 17:53:10 -0500

    a

--- config/database.ts
+++ config/database.ts
@@ -12,7 +12,10 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://theobelo25:ahhVM8sCQN8V7qmX@cluster0.1kdjvrd.mongodb.net/PropertyPulse?retryWrites=true&w=majority&appName=Cluster0"
+    );
     connected = true;
   } catch (error) {
     console.log(error);
